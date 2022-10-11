5f87c0eb446e0aa9f87f06d7fd2b8896000467f9
Josue-Santos-Pinto <js.pinto55@gmail.com>
2022-10-11 11:21:34 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+    background-color: #8000ff;
+`
+const Text = styled.Text``
+
+export default () => {
+    <Container>
+        <Text>Header do App</Text>
+    </Container>
+}
